247c8f2f3676f638900256349b161ed1fa0779a6
Kim kippum <t9cl3x@gmail.com>
2023-05-09 20:39:02 +0900

    230509_실습3~5ㅎ

---
+++ js/jsexam04.js
@@ -0,0 +1,41 @@
+// function changehtmlLine() {
+//     let span = document.getElementsByClassName("content");
+//     span.style.backgroundColor = '#F9DCC4';
+//     span.style.borderColor = '#006D77';
+//     span.style.borderStyle = 'dashed';
+// }
+
+function changehtmlLine(span) {
+    // let span = document.getElementsByClassName("content");
+    span.style.backgroundColor = '#F9DCC4';
+    span.style.borderColor = '#006D77';
+    span.style.borderStyle = 'dashed';
+}
+
+// function changecssLine() {
+//     let span = document.getElementById("textcss");
+//     span.style.backgroundColor = '#F9DCC4';
+//     span.style.borderColor = '#006D77';
+//     span.style.borderStyle = 'dashed';
+// }
+
+// function changejavascriptLine() {
+//     let span = document.getElementById("textjavascript");
+//     span.style.backgroundColor = '#F9DCC4';
+//     span.style.borderColor = '#006D77';
+//     span.style.borderStyle = 'dashed';
+// }
+
+function changebyhtmlLeave(span) {
+    span.style.backgroundColor = '#F6BD60';
+    span.style.borderStyle = 'none';
+}
+
+let x = null;
+function changebyClick(span) {
+    if(x !== null){
+       changebyhtmlLeave(x);
+    }
+    x = span;
+    changehtmlLine(x);
+}
